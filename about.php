d610c6543320577f75300c5b0bad68b98da846e9
AdityaSinghChauhan <astroadityachauhan@gmail.com>
2016-08-15 12:38:02 +0530

    Adding initial files

---
+++ about.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>The Astro Club</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" type="text/css" href="css/mobile.css">
+	<script src="js/mobile.js" type="text/javascript"></script>
+</head>
+<body>
+	<div id="page">
+		<div id="header">
+			<div>
+				
+				<ul id="navigation">
+					<li>
+						<a href="index.php">Home</a>
+					</li>
+					<li>
+						<a href="about.php">About Us</a>
+					</li>
+					<li class="menu">
+						<a href="activities.php">Activities</a>
+					</li>
+					<li class="menu">
+						<a href="blog.php">Blog</a>
+					</li>
+                                        <li class="menu">
+						<a href="downloads.php">Downloads</a>
+					</li>
+                                        <li class="menu">
+						<a href="gallery.php">Gallery</a>
+					</li>
+					<li>
+						<a href="contact.php">Contact</a>
+					</li>
+                                        <li>
+						<a href="login.php">Login</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div id="body" class="about">
+			<div class="header">
+				<div>
+					<h1>About</h1>
+					<h2>We love Astronomy</h2>
+					<p>The Astro Club is a special venture for the popularization of astronomy and basic sciences. It is a joint initiative by Chhattisgarh Vigyan Sabha and Bhilai Institute if Technology, Durg towards eradication 
+                                            of superstitions from amongst the society and inculcate scientific temperament amongst the youth. Started in 2014, The Astro Club is the <b>1st Formal Astronomy Club of Chhattisgarh State </b>
+                                            and is a family of 600+ members currently. </p>
+                                </div>
+			</div>
+			<div class="body">
+				<div>
+					<img src="images/earth-satellite.jpg" alt="">
+					<h2>We love doing fun stuff !</h2>
+                                        <p>We are a team who believes in 'Learning while Playing'. We organize Guest Lectures by Eminent Scientists, Workshops, Night Sky Camps, Collaborative assignments with various scientific bodies, Science Popularization Activities in Remote Villages and a lot more... </p>
+                                </div>
+			</div>
+			<div class="footer">
+				<div>
+					<img src="images/space-shuttle.png" alt="">
+					<h2>Be Part of Our Community</h2>
+                                        <p>If you are passionate about science or want to know about the universe you live in or enjoy looking at the sky, You can be a part of <b>The Astro Club</b> by registering <a href='register.php'>here</a>...</p>
+                                </div>
+			</div>
+			<div class="section">
+                            <div>
+					<img src="images/space-shuttle.png" alt="">
+                            </div>
+				<div>
+					<h2>Our Affiliations</h2>
+                                        <p>
+                                        <ul class="affiliates">
+                                            <li>
+                                                Vigyan Prasar, Dept. of Sciecne and Technology, Govt. of India
+                                            </li>
+                                            <li>
+                                                Chhattisgarh Vigyan Sabha, Raipur
+                                            </li>
+                                             <li>
+                                                All India People Science Network
+                                            </li>
+                                        </ul>
+                                </p>
+                                </div>
+                            <div>
+					<img src="images/space-shuttle.png" alt="">
+                            </div>
+			</div>
+		</div>
+		<div id="footer">
+			<div class="connect">
+				<div>
+					<h1>FOLLOW OUR  MISSIONS ON</h1>
+					<div>
+						<a href="https://www.facebook.com/ASTROatBITD/" class="facebook">facebook</a>
+						<a href="http://freewebsitetemplates.com/go/twitter/" class="twitter">twitter</a>
+						<a href="http://freewebsitetemplates.com/go/googleplus/" class="googleplus">googleplus</a>
+					
+					</div>
+				</div>
+			</div>
+			<div class="footnote">
+				<div>
+					<p>&copy; 2015 - <?php echo date("Y"); ?> The Astro Club | ALL RIGHTS RESERVED</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>The Astro Club</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" type="text/css" href="css/mobile.css">
+	<script src="js/mobile.js" type="text/javascript"></script>
+</head>
+<body>
+	<div id="page">
+		<div id="header">
+			<div>
+				
+				<ul id="navigation">
+					<li>
+						<a href="index.php">Home</a>
+					</li>
+					<li>
+						<a href="about.php">About Us</a>
+					</li>
+					<li class="menu">
+						<a href="activities.php">Activities</a>
+					</li>
+					<li class="menu">
+						<a href="blog.php">Blog</a>
+					</li>
+                                        <li class="menu">
+						<a href="downloads.php">Downloads</a>
+					</li>
+                                        <li class="menu">
+						<a href="gallery.php">Gallery</a>
+					</li>
+					<li>
+						<a href="contact.php">Contact</a>
+					</li>
+                                        <li>
+						<a href="login.php">Login</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div id="body" class="home">
+			<div class="header">
+				<div>
+					<img src="images/satellite.png" alt="" class="satellite">
+					<h1>The Astro Club</h1>
+					
+					<h3>FEATURED ACTIVITIES</h3>
+					<ul>
+						<li>
+                                                    <a href="activities.php"><img src="images/aditya_astro_patrika(1).jpg" alt="" height="156" width="235"></a>
+						</li>
+						<li>
+							<a href="activities.php"><img src="images/aditya_astro_patrika(1).jpg" alt="" height="156" width="235"></a>
+						</li>
+						<li>
+							<a href="activities.php"><img src="images/aditya_astro_patrika(1).jpg" alt="" height="156" width="235"></a>
+						</li>
+						<li>
+							<a href="activities.php"><img src="images/aditya_astro_patrika(1).jpg" alt="" height="156" width="235"></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="body">
+				<div>
+					<h1>OUR MISSION</h1>
+                                        <p>Eradication of superstitions from amongst the society and to inculcate scientific temperament in the youth</p>
+                                </div>
+			</div>
+			<div class="footer">
+				<div>
+					<ul>
+						<li>
+							<h1>Facebook</h1>
+							<a href="blog.html"><img src="images/mars-rover.jpg" alt=""><span></span></a>
+						</li>
+                                                <li>
+							<h1>What's New</h1>
+							<a href="blog.html"><img src="images/mars-rover.jpg" alt=""><span></span></a>
+						</li>
+						<li>
+							<h1>LATEST BLOG</h1>
+							<ul>
+								<li>
+									<a href="blog.html"><img src="images/finding-planet.jpg" alt=""></a>
+									<h1>FINDING PLANET X-123</h1>
+									<span>FEBRUARY 6, 2023</span>
+									<a href="blog.html" class="more">Read More</a>
+								</li>
+								<li>
+									<a href="blog.html"><img src="images/new-satellitedish.jpg" alt=""></a>
+									<h1>NEW SATELLITE DISH</h1>
+									<span>FEBRUARY 3, 2023</span>
+									<a href="blog.html" class="more">Read More</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="footer">
+			<div class="connect">
+				<div>
+					<h1>FOLLOW OUR  MISSIONS ON</h1>
+					<div>
+						<a href="https://www.facebook.com/ASTROatBITD/" class="facebook">facebook</a>
+						<a href="http://freewebsitetemplates.com/go/twitter/" class="twitter">twitter</a>
+						<a href="http://freewebsitetemplates.com/go/googleplus/" class="googleplus">googleplus</a>
+					
+					</div>
+				</div>
+			</div>
+			<div class="footnote">
+				<div>
+					<p>&copy; 2015 - <?php echo date("Y"); ?> The Astro Club | ALL RIGHTS RESERVED</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
