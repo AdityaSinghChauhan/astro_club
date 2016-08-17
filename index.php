1d48efcc37c56091022de6870de6b5d6070c4e5b
AdityaSinghChauhan <astroadityachauhan@gmail.com>
2016-08-17 11:54:50 +0530

    menu navigation dropdown added

--- index.php
+++ index.php
@@ -1,30 +1,26 @@
 <!doctype html>
 <!-- Website template by freewebsitetemplates.com -->
 <?php
-       
-        session_start();
-        $log="Login";
-        $link="login.php";
-       if(isset($_SESSION['SESS_FIRST_NAME']))
-       {
-           $log="Logout ".$_SESSION['SESS_FIRST_NAME'];
-           $link="logout.php";
-           
-       }
-       include 'connection.php';
-       $query=mysql_query("select * from notice_astro ");
-        $row=mysql_fetch_array($query);
-        $notice1=$row['notice_1'];
-        $link1=$row['link_1'];
-        $notice2=$row['notice_2'];
-        $link2=$row['link_2'];
-        $notice3=$row['notice_3'];
-        $link3=$row['link_3'];
-        $notice4=$row['notice_4'];
-        $link4=$row['link_4'];
-        $notice5=$row['notice_5'];
-        $link5=$row['link_5'];
-       
+session_start();
+$log = "Login";
+$link = "login.php";
+if (isset($_SESSION['SESS_FIRST_NAME'])) {
+    $log = "Logout " . $_SESSION['SESS_FIRST_NAME'];
+    $link = "logout.php";
+}
+include 'connection.php';
+$query = mysql_query("select * from notice_astro ");
+$row = mysql_fetch_array($query);
+$notice1 = $row['notice_1'];
+$link1 = $row['link_1'];
+$notice2 = $row['notice_2'];
+$link2 = $row['link_2'];
+$notice3 = $row['notice_3'];
+$link3 = $row['link_3'];
+$notice4 = $row['notice_4'];
+$link4 = $row['link_4'];
+$notice5 = $row['notice_5'];
+$link5 = $row['link_5'];
 ?>
 <html>
     <head>
@@ -36,67 +32,67 @@
         <script src="js/mobile.js" type="text/javascript"></script>
         <style type="text/css">
 
-#marqueecontainer{
-position: relative;
-width: 400px; /*marquee width */
-height: 250px; /*marquee height */
-background-color: transparent;
-overflow: hidden;
-border: 1px solid whitesmoke;
-padding: 2px;
-padding-left: 4px;
+            #marqueecontainer{
+                position: relative;
+                width: 400px; /*marquee width */
+                height: 250px; /*marquee height */
+                background-color: transparent;
+                overflow: hidden;
+                border: 1px solid whitesmoke;
+                padding: 2px;
+                padding-left: 4px;
 
-}
+            }
 
-</style>
-
+        </style>
+       
         <script type="text/javascript">
 
-/***********************************************
-* Cross browser Marquee II- © Dynamic Drive (www.dynamicdrive.com)
-* This notice MUST stay intact for legal use
-* Visit http://www.dynamicdrive.com/ for this script and 100s more.
-***********************************************/
+            /***********************************************
+             * Cross browser Marquee II- © Dynamic Drive (www.dynamicdrive.com)
+             * This notice MUST stay intact for legal use
+             * Visit http://www.dynamicdrive.com/ for this script and 100s more.
+             ***********************************************/
 
-var delayb4scroll=2000 //Specify initial delay before marquee starts to scroll on page (2000=2 seconds)
-var marqueespeed=2 //Specify marquee scroll speed (larger is faster 1-10)
-var pauseit=1 //Pause marquee onMousever (0=no. 1=yes)?
+            var delayb4scroll = 2000 //Specify initial delay before marquee starts to scroll on page (2000=2 seconds)
+            var marqueespeed = 2 //Specify marquee scroll speed (larger is faster 1-10)
+            var pauseit = 1 //Pause marquee onMousever (0=no. 1=yes)?
 
 ////NO NEED TO EDIT BELOW THIS LINE////////////
 
-var copyspeed=marqueespeed
-var pausespeed=(pauseit==0)? copyspeed: 0
-var actualheight=''
+            var copyspeed = marqueespeed
+            var pausespeed = (pauseit == 0) ? copyspeed : 0
+            var actualheight = ''
 
-function scrollmarquee(){
-if (parseInt(cross_marquee.style.top)>(actualheight*(-1)+8))
-cross_marquee.style.top=parseInt(cross_marquee.style.top)-copyspeed+"px"
-else
-cross_marquee.style.top=parseInt(marqueeheight)+8+"px"
-}
+            function scrollmarquee() {
+                if (parseInt(cross_marquee.style.top) > (actualheight * (-1) + 8))
+                    cross_marquee.style.top = parseInt(cross_marquee.style.top) - copyspeed + "px"
+                else
+                    cross_marquee.style.top = parseInt(marqueeheight) + 8 + "px"
+            }
 
-function initializemarquee(){
-cross_marquee=document.getElementById("vmarquee")
-cross_marquee.style.top=0
-marqueeheight=document.getElementById("marqueecontainer").offsetHeight
-actualheight=cross_marquee.offsetHeight
-if (window.opera || navigator.userAgent.indexOf("Netscape/7")!=-1){ //if Opera or Netscape 7x, add scrollbars to scroll and exit
-cross_marquee.style.height=marqueeheight+"px"
-cross_marquee.style.overflow="scroll"
-return
-}
-setTimeout('lefttime=setInterval("scrollmarquee()",30)', delayb4scroll)
-}
+            function initializemarquee() {
+                cross_marquee = document.getElementById("vmarquee")
+                cross_marquee.style.top = 0
+                marqueeheight = document.getElementById("marqueecontainer").offsetHeight
+                actualheight = cross_marquee.offsetHeight
+                if (window.opera || navigator.userAgent.indexOf("Netscape/7") != -1) { //if Opera or Netscape 7x, add scrollbars to scroll and exit
+                    cross_marquee.style.height = marqueeheight + "px"
+                    cross_marquee.style.overflow = "scroll"
+                    return
+                }
+                setTimeout('lefttime=setInterval("scrollmarquee()",30)', delayb4scroll)
+            }
 
-if (window.addEventListener)
-window.addEventListener("load", initializemarquee, false)
-else if (window.attachEvent)
-window.attachEvent("onload", initializemarquee)
-else if (document.getElementById)
-window.onload=initializemarquee
+            if (window.addEventListener)
+                window.addEventListener("load", initializemarquee, false)
+            else if (window.attachEvent)
+                window.attachEvent("onload", initializemarquee)
+            else if (document.getElementById)
+                window.onload = initializemarquee
 
 
-</script>
+        </script>
     </head>
     <body>
         <div id="fb-root"></div>
@@ -117,11 +113,21 @@ window.onload=initializemarquee
                         <li>
                             <a href="index.php">Home</a>
                         </li>
-                        <li>
-                            <a href="about.php">About Us</a>
+                        <li class="menu">
+                            <a href="#" >About Us</a>
+                            <ul class="primary">
+                                <li><a href="about.php">About Us</a></li>
+                                <li><a href="team.php">Our Team</a></li>
+                                <li><a href="registration.php">Be a Part</a></li>
+                            </ul>
                         </li>
                         <li class="menu">
-                            <a href="activities.php">Activities</a>
+                            <a href="#" data-flexmenu="activity">Activities</a>
+                            <ul class="primary">
+                                <li><a href="activities.php">Our Activities</a></li>
+                                <li><a href="assignments.php">Our Assignments</a></li>
+                                <li><a href="/wp">Blog</a></li>
+                            </ul>
                         </li>
                         <li class="menu">
                             <a href="blog.php">Blog</a>
@@ -198,34 +204,34 @@ window.onload=initializemarquee
                             </li>
                             <li>
                                 <h1>Twitter</h1>
-<!--                                <ul>
-                                    <li>
-                                        <a href="blog.html"><img src="images/finding-planet.jpg" alt=""></a>
-                                        <h1>FINDING PLANET X-123</h1>
-                                        <span>FEBRUARY 6, 2023</span>
-                                        <a href="blog.html" class="more">Read More</a>
-                                    </li>
-                                    <li>
-                                        <a href="blog.html"><img src="images/new-satellitedish.jpg" alt=""></a>
-                                        <h1>NEW SATELLITE DISH</h1>
-                                        <span>FEBRUARY 3, 2023</span>
-                                        <a href="blog.html" class="more">Read More</a>
-                                    </li>
-                                </ul>-->
-<br><br>
-<a class="twitter-timeline" data-width="400" data-height="300" data-theme="dark" href="https://twitter.com/The_Astro_Club">Tweets by Astro Club</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+                                <!--                                <ul>
+                                                                    <li>
+                                                                        <a href="blog.html"><img src="images/finding-planet.jpg" alt=""></a>
+                                                                        <h1>FINDING PLANET X-123</h1>
+                                                                        <span>FEBRUARY 6, 2023</span>
+                                                                        <a href="blog.html" class="more">Read More</a>
+                                                                    </li>
+                                                                    <li>
+                                                                        <a href="blog.html"><img src="images/new-satellitedish.jpg" alt=""></a>
+                                                                        <h1>NEW SATELLITE DISH</h1>
+                                                                        <span>FEBRUARY 3, 2023</span>
+                                                                        <a href="blog.html" class="more">Read More</a>
+                                                                    </li>
+                                                                </ul>-->
+                                <br><br>
+                                <a class="twitter-timeline" data-width="400" data-height="300" data-theme="dark" href="https://twitter.com/The_Astro_Club">Tweets by Astro Club</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
                             </li>
                             <li>
                                 <h1>Associated Bodies</h1>
                                 <br><br>
-                               <ul>
-                                   <li><a href="http://cgvigyansabha.in" target="_blank"><h1>Chhattigarh Vigyan Sabha</h1></a></li>
-                                  <li><a href="http://vigyanprasar.gov.in" target="_blank"><h1> Vigyan Prasar</h1></a></li>
-                                  <li><a href="http://www.aipsc.in/" target="_blank"><h1> AIPSN</h1></a></li>
-                                  <li><a href="http://www.astron-soc.in/" target="_blank"><h1> A.S.I.</h1></a></li>
-                                  
+                                <ul>
+                                    <li><a href="http://cgvigyansabha.in" target="_blank"><h1>Chhattigarh Vigyan Sabha</h1></a></li>
+                                    <li><a href="http://vigyanprasar.gov.in" target="_blank"><h1> Vigyan Prasar</h1></a></li>
+                                    <li><a href="http://www.aipsc.in/" target="_blank"><h1> AIPSN</h1></a></li>
+                                    <li><a href="http://www.astron-soc.in/" target="_blank"><h1> A.S.I.</h1></a></li>
+
                                 </ul>
-<br><br>
+                                <br><br>
                             </li>
                         </ul>
                     </div>
@@ -251,4 +257,10 @@ window.onload=initializemarquee
             </div>
         </div>
     </body>
-</html>
+    <ul id="about" class="flexdropdownmenu">
+
+        <ul>
+            <ul id="activity" class="flexdropdownmenu">
+
+                <ul>
+                    </html>
